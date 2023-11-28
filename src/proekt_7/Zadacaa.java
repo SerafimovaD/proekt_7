1c23d78b062323c0db7ff5074b58cd90f92af1c1
SerafimovaD <daci.serafimova@gmail.com>
2023-11-28 13:32:16 +0100

    Add first java program

---
+++ src/proekt_7/Zadacaa.java
@@ -0,0 +1,14 @@
+package proekt_7;
+
+public class Zadacaa {
+
+	public static void main(String[] args) {
+		int x=10;
+		int y=myFunc(x);
+		System.out.println(y);
+	}
+	public static int myFunc(int x) {
+		return x*3;
+	}
+
+}
---
+++ src/proekt_7/Zadacaaa.java
@@ -0,0 +1,12 @@
+package proekt_7;
+
+public class Zadacaaa {
+
+	public static void main(String[] args) {
+		myFunc(10);
+	}
+	public static void myFunc(int x) {
+		int y=x*3;
+		System.out.println(y);
+	}
+}
